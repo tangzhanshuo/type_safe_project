a2f1e23bd6064d9257d51d6eb56107bec649f94a
AAA <3225329724@qq.com>
2024-07-07 22:45:19 +0800

    新的course structure，建议把course data删了重跑一次，鉴于前端正在修改，示例暂不更新

--- course/src/main/scala/Common/CourseAPI/UpdateCourseMessage.scala
+++ course/src/main/scala/Common/CourseAPI/UpdateCourseMessage.scala
@@ -1,7 +1,17 @@
 package Common.CourseAPI
 
 import Common.API.API
-import Common.Object.SqlParameter
 import Global.ServiceCenter.courseServiceCode
 
-case class UpdateCourseMessage(courseID: Int, courseName: Option[String], teacherUsername: Option[String], teacherName: Option[String], capacity: Option[Int]) extends API[String](courseServiceCode)
+case class UpdateCourseMessage(
+                                courseID: Int,
+                                courseName: Option[String],
+                                teacherUsername: Option[String],
+                                teacherName: Option[String],
+                                capacity: Option[Int],
+                                info: Option[String],
+                                courseHourJson: Option[String], // JSON represented as String
+                                credits: Option[Int],
+                                enrolledStudentsJson: Option[String], // JSON represented as String
+                                kwargsJson: Option[String] // JSON represented as String
+                              ) extends API[String](courseServiceCode)
--- teacher/src/main/scala/Common/CourseAPI/package.scala
+++ teacher/src/main/scala/Common/CourseAPI/package.scala
@@ -4,15 +4,26 @@ import Common.API.{API, PlanContext, TraceID}
 import Common.Object.SqlParameter
 import Global.ServiceCenter.courseServiceCode
 import cats.effect.IO
-import io.circe.Json
 import io.circe.generic.auto.*
 import io.circe.syntax.*
 import org.http4s.client.Client
+import io.circe.Json
 
 package object CourseAPI {
 
-  def addCourse(courseID: Int, courseName: String, teacherUsername: String, teacherName: String, capacity: Int)(using PlanContext): IO[String] =
-    AddCourseMessage(courseID, courseName, teacherUsername, teacherName, capacity).send
+  def addCourse(
+                 courseID: Int,
+                 courseName: String,
+                 teacherUsername: String,
+                 teacherName: String,
+                 capacity: Int,
+                 info: String,
+                 courseHourJson: String,
+                 credits: Int,
+                 enrolledStudentsJson: String,
+                 kwargsJson: String
+               )(using PlanContext): IO[String] =
+    AddCourseMessage(courseID, courseName, teacherUsername, teacherName, capacity, info, courseHourJson, credits, enrolledStudentsJson, kwargsJson).send
 
   def deleteCourse(courseID: Int)(using PlanContext): IO[String] =
     DeleteCourseMessage(courseID).send
@@ -20,6 +31,20 @@ package object CourseAPI {
   def getCourse(courseID: Int)(using PlanContext): IO[String] =
     GetCourseMessage(courseID).send
 
-  def updateCourse(courseID: Int, courseName: Option[String], teacherUsername: Option[String], teacherName: Option[String], capacity: Option[Int])(using PlanContext): IO[String] =
-    UpdateCourseMessage(courseID, courseName, teacherUsername, teacherName, capacity).send
+  def updateCourse(
+                    courseID: Int,
+                    courseName: Option[String],
+                    teacherUsername: Option[String],
+                    teacherName: Option[String],
+                    capacity: Option[Int],
+                    info: Option[String],
+                    courseHourJson: Option[String],
+                    credits: Option[Int],
+                    enrolledStudentsJson: Option[String],
+                    kwargsJson: Option[String]
+                  )(using PlanContext): IO[String] =
+    UpdateCourseMessage(courseID, courseName, teacherUsername, teacherName, capacity, info, courseHourJson, credits, enrolledStudentsJson, kwargsJson).send
+
+  def addStudent2Course(courseID: Int, studentUsername: Option[String])(using PlanContext): IO[String] =
+    AddStudent2CourseMessage(courseID, studentUsername).send
 }
