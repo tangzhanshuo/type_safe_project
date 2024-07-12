98724b548b22e309d0bc20e1d2d9192a16452133
tangzhanshuo <tangzs23@mails.tsinghua.edu.cn>
2024-07-12 10:11:21 +0800

    why is there still a s

--- admin/src/main/scala/Impl/AdminGetCoursesByStudentUsernameMessagePlanner.scala
+++ admin/src/main/scala/Impl/AdminGetCoursesByStudentUsernameMessagePlanner.scala
@@ -2,13 +2,13 @@ package Impl
 
 import cats.effect.IO
 import Common.API.{PlanContext, Planner}
-import Common.CourseAPI.getCoursesByStudentUsername
+import Common.CourseAPI.getCourseByStudentUsername
 
 case class AdminGetCoursesByStudentUsernameMessagePlanner(
                                                  studentUsername: String,
                                                  override val planContext: PlanContext
                                                ) extends Planner[String] {
   override def plan(using planContext: PlanContext): IO[String] = {
-    getCoursesByStudentUsername(studentUsername)
+    getCourseByStudentUsername(studentUsername)
   }
 }
