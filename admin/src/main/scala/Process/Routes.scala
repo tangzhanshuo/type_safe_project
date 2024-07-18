87a5febdf0e4354b6b30067063eaef749b439726
tangzhanshuo <tangzs23@mails.tsinghua.edu.cn>
2024-07-18 20:02:08 +0800

    why is this code still here

--- admin/src/main/scala/Process/Routes.scala
+++ admin/src/main/scala/Process/Routes.scala
@@ -12,11 +12,6 @@ import org.http4s.dsl.io.*
 object Routes:
   private def executePlan(messageType: String, str: String): IO[String] =
     messageType match {
-      case "UserLoginMessage" =>
-        IO(decode[UserLoginMessagePlanner](str).getOrElse(throw new Exception("Invalid JSON for UserLoginMessage")))
-          .flatMap { m =>
-            m.fullPlan.map(_.asJson.noSpaces)
-          }
       case "AdminAddCourseMessage" =>
         IO(decode[AdminAddCourseMessagePlanner](str).getOrElse(throw new Exception("Invalid JSON for AdminAddCourseMessage")))
           .flatMap { m =>
